74d611b412988a821bf097a9f040906e5a7177cc
lameshan <62106334+lameshan@users.noreply.github.com>
2021-06-01 12:04:40 +0530

    dd

--- src/screens/home/Search.js
+++ src/screens/home/Search.js
@@ -4,7 +4,24 @@ import "./Search.css";
 const Search = ({ searchData }) => {
   return (
     <React.Fragment>
-    
+      <div className="d-flex justify-content-center h-100">
+        <div className="searchbar">
+          <input
+            className="search_input"
+            type="text"
+            name=""
+            placeholder="Search..."
+            onChange={(e) => searchData(e.target.value)}
+          />
+
+          <SearchIcon
+            className="search_icon"
+            onClick={() => {
+              console.log("Clicked..");
+            }}
+          />
+        </div>
+      </div>
     </React.Fragment>
   );
 };
