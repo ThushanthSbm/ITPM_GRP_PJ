d41e5d6a4b14d2949c4e5b6a55dfaf413efcf864
ThenojanAnandam <55612934+ThenojanAnandam@users.noreply.github.com>
2021-06-02 12:22:14 +0530

    change table

--- src/screens/year_semister/YearSemisterTable.js
+++ src/screens/year_semister/YearSemisterTable.js
@@ -7,39 +7,7 @@ const YearSemisterTable = ({
   gotoUpdatePage,
 }) => {
   return (
-    <table className="table table-dark table-hover YearViewContainer__table">
-      <thead>
-        <tr>
-          <th>Year&Semister</th>
-          <th>Action</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {userData.map((data) => (
-          <tr key={data.id}>
-            <td>
-              <div className="form-check">
-                <input
-                  onChange={(e) => Handlebox(e, data)}
-                  type="checkbox"
-                  value={data.id}
-                  className="form-check-input"
-                  checked={data.isChecked}
-                />
-                <span>{data.year_semister}</span>
-              </div>
-            </td>
-            <td>
-              <button onClick={(e) => handleDelete(data)}>Delete</button>
-            </td>
-            <td>
-              <button onClick={(e) => gotoUpdatePage(data)}>Edit</button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+   
   );
 };
 
