bd4cbf32da5d889d73486d05e6db55a60b37f337
ThenojanAnandam <55612934+ThenojanAnandam@users.noreply.github.com>
2021-06-02 12:21:14 +0530

    change

--- src/screens/year_semister/Yearsemister.js
+++ src/screens/year_semister/Yearsemister.js
@@ -42,7 +42,55 @@ const Yearsemister = () => {
   ];
 
   return (
+    <div className="yearSemister">
+      <ScreenNav rightNavData={navData} />
+      <div className="yearSemister__container">
+        <div className="yearSemister__box">
+          <div className="lead text-success yearSemister__message">
+            {loading && clicked && <Spinner Loader={DotLoader} size={30} />}
+            <p className={`lead ${error ? "text-danger" : "text-success"}`}>
+              {!loading && !error && success}
+              {!loading && error && error}
+            </p>
+          </div>
 
+          <h2 className="text-center text-dark">Add year & Semister</h2>
+          <form id="frm" onSubmit={(e) => submitHandler(e)}>
+            <div className="yearSemister_inputs">
+              <label htmlFor="year_semister" className="text">
+                year & semister
+              </label>
+              <input
+                value={year}
+                onChange={(e) => setYear(e.target.value)}
+                placeholder="eg:S1.Y3"
+                id="year_semister"
+                type="text"
+                className="form-control"
+                required
+              />
+            </div>
+            <div className="yearSemister_buttons">
+              <button type="submit" className="btn" disabled={!year}>
+                Add
+              </button>
+              <button
+                type="button"
+                className="btn"
+                onClick={(e) => {
+                  history.push({ pathname: "/student/year_semister/view" });
+                }}
+              >
+                View
+              </button>
+              <button type="button" onClick={clearInput} className="btn">
+                Clear
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
