fc89a785c5ce8cb7d02e65c8a9c813907f93342d
lameshan <62106334+lameshan@users.noreply.github.com>
2021-06-02 11:12:56 +0530

    view

--- src/screens/notAllocated/ViewNotAllocated.js
+++ src/screens/notAllocated/ViewNotAllocated.js
@@ -11,7 +11,11 @@ const ViewNotAllocated = () => {
   const { loading, error, not_allocates } = useSelector(
     (state) => state.get_notAllocated
   );
+  //   console.log("gen_subgroupids", gen_subgroupids);
 
+  useEffect(() => {
+    dispatch(view_notAlocatedTime());
+    return () => {};
   }, []);
   useEffect(() => {
     setSession(not_allocates);
