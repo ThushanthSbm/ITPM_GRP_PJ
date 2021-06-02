440fc47d99d43ae56021784acbd7da5acf16801a
lameshan <62106334+lameshan@users.noreply.github.com>
2021-06-02 11:11:18 +0530

    aa

--- src/screens/home/Search.js
+++ src/screens/home/Search.js
@@ -3,16 +3,6 @@ import SearchIcon from "@material-ui/icons/Search";
 import "./Search.css";
 const Search = ({ searchData }) => {
   return (
-    <React.Fragment>
-      <div className="d-flex justify-content-center h-100">
-        <div className="searchbar">
-          <input
-            className="search_input"
-            type="text"
-            name=""
-            placeholder="Search..."
-            onChange={(e) => searchData(e.target.value)}
-          />
 
           <SearchIcon
             className="search_icon"
