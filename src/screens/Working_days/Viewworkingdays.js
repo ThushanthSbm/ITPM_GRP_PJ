f6e68b0aaf4df321f7798cf8d127d5f19d769ed4
lameshan <62106334+lameshan@users.noreply.github.com>
2021-06-03 09:54:33 +0530

    wd

--- src/screens/Working_days/Viewworkingdays.js
+++ src/screens/Working_days/Viewworkingdays.js
@@ -31,9 +31,7 @@ const Viewworkingdays = () => {
       pathname: "/",
     },
     {
-      id: 2,
-      name: "View Working Days and Hours",
-      pathname: "/workingdays/view",
+   
     },
   ];
 
