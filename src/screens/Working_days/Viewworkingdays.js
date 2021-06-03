d58dd28e1c9a288bd706ef927bc88d1bcff95318
lameshan <62106334+lameshan@users.noreply.github.com>
2021-06-03 09:56:32 +0530

    Update Viewworkingdays.js

--- src/screens/Working_days/Viewworkingdays.js
+++ src/screens/Working_days/Viewworkingdays.js
@@ -79,6 +79,11 @@ const Viewworkingdays = () => {
     setCheckData([]);
   };
 
+  const handleDelete = (data) => {
+    db.collection("workingdays").doc(data.id).delete();
+    setCheckData(checkData.filter((e) => e.id !== data.id));
+    console.log("checkData", checkData);
+  };
 
   const searchData = (name) => {
     setCheckData([]);
