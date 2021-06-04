73536b307eacc06f73f4bb3dc8b83ff683a362a7
kobinath28 <61456871+kobinath28@users.noreply.github.com>
2021-06-04 23:23:59 +0530

    change div in room.js

--- src/screens/Room/Room.js
+++ src/screens/Room/Room.js
@@ -76,18 +76,7 @@ const Room = () => {
               />
             </div>
 
-            <div className="room_inputs">
-              <label htmlFor="capacity">Capacity</label>
-              <input
-                placeholder="eg:-150"
-                name="capacity"
-                type="text"
-                className="form-control"
-                value={formik.values.capacity}
-                onChange={formik.handleChange}
-                required
-              />
-            </div>
+            
 
             <div className="room_inputs">
               <label htmlFor="building">Building</label>
